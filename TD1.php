c0f3a0433b4aad4c143c3798989a840898a9ef29
MurielParaire <muriel.paraire@etu.univ-amu.fr>
2021-11-22 08:35:50 +0100

    debut

---
+++ TD1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+    echo '<strong> Voici mon premier programme PHP </strong> <br/>' . "\n";
+    echo 'C\'est pas mal non ? <br/>';
+?>
+</body>
+</html>
